693d9f37776c4f86494fc85ced4085a7aef5a04c
LOKENDRA SINGH <119702812+Guddubhaiya07@users.noreply.github.com>
2025-07-29 22:22:59 +0530

    Create Day04.py

---
+++ Day04.py
@@ -0,0 +1,18 @@
+#ASCII Range Sum
+class Solution:
+    def asciirange(self, s: str) -> list:
+        result = []
+        n = len(s)
+        
+        for ch in set(s):
+            first = s.find(ch)
+            last = s.rfind(ch)
+            
+            if first != last:
+                sum_ascii = 0
+                for i in range(first + 1, last):
+                    sum_ascii += ord(s[i])
+                if sum_ascii > 0:
+                    result.append(sum_ascii)
+        
+        return result
